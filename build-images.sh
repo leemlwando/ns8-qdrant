f50fe926e31dce48dec4b6062ca8f046292a8bfc
leemlwando <leemlwando@gmail.com>
2025-06-15 11:38:05 +0200

    Fix GitHub Actions workflows and build script
    - Fixed duplicated code in build-images.sh
    - Updated to modern GitHub Actions output syntax
    - Recreated clean YAML workflows (build.yml, release.yml, test.yml)
    - Updated test workflow to use yarn instead of pnpm
    - Fixed repository references to use ghcr.io/leemlwando
    - Ensured proper event triggers in all workflows

--- build-images.sh
+++ build-images.sh
@@ -63,23 +63,7 @@ images+=("${repobase}/${reponame}")
 # Setup CI when pushing to Github. 
 # Warning! docker::// protocol expects lowercase letters (,,)
 if [[ -n "${CI}" ]]; then
-    # Set output value for Github Actions
-    printf "::set-output name=images::%s\n" "${images[*],,}"
-else
-    # Just print info for manual push
-    printf "Publish the images with:\n\n"
-    for image in "${images[@],,}"; do printf "  buildah push %s docker://%s:%s\n" "${image}" "${image}" "${IMAGETAG:-latest}" ; done
-    printf "\n"
-fi
-
-# Append the image URL to the images array
-images+=("${repobase}/${reponame}")
-
-#
-# Setup CI when pushing to Github. 
-# Warning! docker::// protocol expects lowercase letters (,,)
-if [[ -n "${CI}" ]]; then
-    # Set output value for Github Actions
+    # Set output value for Github Actions (using modern syntax)
     printf "images=%s\n" "${images[*],,}" >> "${GITHUB_OUTPUT}"
 else
     # Just print info for manual push
